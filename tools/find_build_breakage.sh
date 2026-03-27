70ec5390eacaee575dc7dec19d6df50ec37d703c
Betty Zhou <bettyzhou@google.com>
2026-03-26 18:26:42 -0700

    Connecting to WIFI and disabling audio in run_test_only.sh.
    Set default retry to be 2 in in find_build_breakage.sh and
    find_change_breakage.sh.
    
    Run adb disable-verity as best effort in flash_device.sh.
    
    Test: run local test
    Bug: 362316112
    Change-Id: I4edd1604328efa7fb25bac37af4e21f9b5232035

--- tools/find_build_breakage.sh
+++ tools/find_build_breakage.sh
@@ -9,9 +9,9 @@
 # --- Configuration Constants ---
 readonly DEFAULT_BISECT_CONFIG_FILENAME="bisect_builds.xml"
 readonly DEFAULT_OUTPUT_DIR="out/$(date +%Y%m%d_%H%M%S)"
-readonly DEFAULT_TEST_RETRY=3
-readonly DEFAULT_SETUP_RETRY=3
-readonly DEFAULT_DOWNLOAD_RETRY=3
+readonly DEFAULT_TEST_RETRY=2
+readonly DEFAULT_SETUP_RETRY=2
+readonly DEFAULT_DOWNLOAD_RETRY=2
 readonly -A BUILD_TYPE_MAP=(
     ["pb"]="PLATFORM_BUILD"
     ["kb"]="KERNEL_BUILD"
